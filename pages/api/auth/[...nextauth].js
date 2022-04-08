6c3208a6677121b9dd4ff1b687275942fff037c5
Eldar <eldar.hidirow@gmail.com>
2022-04-08 21:23:02 +0300

    My Facebook!!!

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,11 +2,12 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
 export default NextAuth({
+  site: process.env.NEXTAUTH_URL,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_URL,
 });
